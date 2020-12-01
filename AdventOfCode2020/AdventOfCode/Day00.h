12c6a6718584e86ee7fb86173680963c0d466842
TheDwoon <TheDwoon@users.noreply.github.com>
2020-12-01 16:56:35 +0100

    AbstractDay is now header only. Implemented Day01

--- AdventOfCode2020/AdventOfCode/Day00.h
+++ AdventOfCode2020/AdventOfCode/Day00.h
@@ -1,10 +1,10 @@
 #pragma once
-#include "AbstractDay.cpp"
+#include "AbstractDay.h"
 
 class Day00 : public virtual AbstractDay<std::string>
 {
 public:
-	Day00() : AbstractDay("input/day00.txt") {};
+	Day00() : AbstractDay("Day 00", "input/day00.txt") {};
 
 	day_t parseInput(std::string& input) override {
 		return input;
