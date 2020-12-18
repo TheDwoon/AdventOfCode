c9bffae3eb51ed57b48ebdbd513de54d4e312a41
TheDwoon <TheDwoon@users.noreply.github.com>
2020-12-18 21:09:34 +0100

    implemented Day 18

--- AdventOfCode2020/AdventOfCode/Day18.cpp
+++ AdventOfCode2020/AdventOfCode/Day18.cpp
@@ -7,7 +7,8 @@ std::shared_ptr<Node> parseExpression(const std::vector<std::string>& tokens, in
 std::shared_ptr<Node> parseNode(const std::vector<std::string> &tokens, int& pos) {
     std::string token = tokens[pos++];
     if (token == "(") {
-        return parseExpression(tokens, pos);
+        std::shared_ptr<Node> expr = parseExpression(tokens, pos);
+        return std::make_shared<Braces>(expr);
     }
     else {
         return std::make_shared<Literal>(std::stoi(token));
@@ -45,7 +46,7 @@ std::vector<std::shared_ptr<Node>> Day18::parseInput(std::string& input)
     for (std::string line; std::getline(stream, line);) {
         // tokenize single line
         std::vector<std::string> tokens;
-        for (auto it = std::sregex_iterator(input.begin(), input.end(), regex); it != std::sregex_iterator(); ++it) {
+        for (auto it = std::sregex_iterator(line.begin(), line.end(), regex); it != std::sregex_iterator(); ++it) {
             std::smatch match = *it;
             tokens.push_back(match[0].str());
         }
@@ -61,14 +62,64 @@ std::vector<std::shared_ptr<Node>> Day18::parseInput(std::string& input)
 
 std::string Day18::runPart1(day_t& input)
 {
-    std::cout << input.size();
-
     long long sum = 0;
     for (auto& expr : input) {
-        sum += expr->evaluate();
+        long long res = expr->evaluate();
+        //expr->print(std::cout);
+        //std::cout << " = " << res << std::endl;
+        sum += res;
     }
 
     std::ostringstream output;
+    output << sum;
+    return output.str();
+}
+
+std::shared_ptr<Node> checkNode(std::shared_ptr<Node> node) {
+    switch (node->type()) {
+    case BRACES: {
+        std::shared_ptr<Braces> braces = std::static_pointer_cast<Braces>(node);
+        braces->m_node = checkNode(braces->m_node);
+        return node;
+    }
+    case MULTIPLICATION: {
+        std::shared_ptr<Multiplication> mult = std::static_pointer_cast<Multiplication>(node);
+        mult->m_left = checkNode(mult->m_left);
+        mult->m_right = checkNode(mult->m_right);
+        return node;
+    }
+    case ADDITION: {
+        std::shared_ptr<Addition> addition = std::static_pointer_cast<Addition>(node);
+        addition->m_left = checkNode(addition->m_left);
+        addition->m_right = checkNode(addition->m_right);
+        if (addition->needsReordering()) {
+            std::shared_ptr<Multiplication> mult = std::static_pointer_cast<Multiplication>(addition->m_left);
+            std::shared_ptr<Node> ll = mult->m_left;
+            std::shared_ptr<Node> lr = mult->m_right;
+            std::shared_ptr<Node> r = addition->m_right;
+
+            std::shared_ptr<Addition> nAdd = std::make_shared<Addition>(lr, r);
+            std::shared_ptr<Multiplication> nMul = std::make_shared<Multiplication>(ll, nAdd);
+            return nMul;
+        }
+        else {
+            return addition;
+        }
+    }
+    case LITERAL:
+        return node;
+    }
+}
+
+std::string Day18::runPart2(day_t& input)
+{
+    long long sum = 0;
+    for (auto expr : input) {        
+        expr = checkNode(expr);
+        sum += expr->evaluate();
+    }
+
+    std::stringstream output;
     output << sum;
     return output.str();
 }
