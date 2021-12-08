075bde7ef60c827cdc6d3d4190f15b63a3db40d8
TheDwoon <TheDwoon@users.noreply.github.com>
2021-12-08 22:13:59 +0100

    implemented Day 08 Part 2

--- AdventOfCode2021/Day08.cpp
+++ AdventOfCode2021/Day08.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <iterator>
 #include <vector>
+#include <set>
 
 #define TITLE "Day 08"
 
@@ -15,17 +16,6 @@ const uint8_t sig_d = 0b00001000;
 const uint8_t sig_e = 0b00010000;
 const uint8_t sig_f = 0b00100000;
 const uint8_t sig_g = 0b01000000;
-
-const uint8_t digit_0 = sig_a | sig_b | sig_c | sig_e | sig_f | sig_g;
-const uint8_t digit_1 = sig_c | sig_f;
-const uint8_t digit_2 = sig_a | sig_c | sig_d | sig_e | sig_g;
-const uint8_t digit_3 = sig_a | sig_c | sig_d | sig_f | sig_g;
-const uint8_t digit_4 = sig_b | sig_c | sig_d | sig_f;
-const uint8_t digit_5 = sig_a | sig_b | sig_d | sig_f | sig_g;
-const uint8_t digit_6 = sig_a | sig_b | sig_d | sig_e | sig_f | sig_g;
-const uint8_t digit_7 = sig_a | sig_c | sig_f;
-const uint8_t digit_8 = sig_a | sig_b | sig_c | sig_d | sig_e | sig_f | sig_g;
-const uint8_t digit_9 = sig_a | sig_b | sig_c | sig_d | sig_f | sig_g;;
 
 struct ScrambledInformation {
     uint8_t input[14] { 0 };
@@ -135,6 +125,91 @@ std::string runPart1(day_t& input) {
 std::string runPart2(day_t& input) {
     std::stringstream output;
 
+    size_t sum = 0;
+
+    for (const ScrambledInformation &info : input) {
+        // setup decryption
+        std::set<uint8_t> bucket[6];
+        uint8_t a { 0 }, b { 0 }, c { 0 }, d { 0 }, e { 0 }, f { 0 }, g { 0 };
+
+        for (size_t i = 0; i < 14; ++i) {
+            bucket[info.s_input[i] - 2].emplace(info.input[i]);
+        }
+
+        uint8_t d_1 = *(bucket[0].begin());
+        uint8_t d_7 = *(bucket[1].begin());
+        uint8_t d_4 = *(bucket[2].begin());
+        uint8_t d_235_s = bucket[3].size();
+        uint8_t d_235 = 0;
+        for (uint8_t it : bucket[3]) {
+            if (d_235 == 0)
+                d_235 = it;
+            else
+                d_235 &= it;
+        }
+        uint8_t d_069_s = bucket[4].size();
+        uint8_t d_069 = 0;
+        for (uint8_t it : bucket[4]) {
+            if (d_069 == 0)
+                d_069 = it;
+            else
+                d_069 &= it;
+        }
+        uint8_t d_8 = *(bucket[5].begin());
+
+        // logic
+        a = d_1 ^ d_7;
+        d = d_4 & d_235;
+        e = (d_7 | d_4 | d_069) ^ d_8;
+        g = (d_235 & d_069) ^ a;
+        c = static_cast<uint8_t>(~(d_069 & d_8) ^ 0b10000000) ^ d ^ e;
+        f = d_1 ^ c;
+        b = d_4 ^ c ^ d ^ f;
+
+        // decode output
+        const uint8_t digit_0 = a | b | c | e | f | g;
+        const uint8_t digit_1 = c | f;
+        const uint8_t digit_3 = a | c | d | f | g;
+        const uint8_t digit_2 = a | c | d | e | g;
+        const uint8_t digit_4 = b | c | d | f;
+        const uint8_t digit_5 = a | b | d | f | g;
+        const uint8_t digit_6 = a | b | d | e | f | g;
+        const uint8_t digit_7 = a | c | f;
+        const uint8_t digit_8 = a | b | c | d | e | f | g;
+        const uint8_t digit_9 = a | b | c | d | f | g;
+
+        uint16_t decoded[4] { 0 };
+        for (size_t i = 0; i < 4; i++) {
+            uint8_t codedOutput = info.input[10 + i];
+            if (codedOutput == digit_0)
+                decoded[i] = 0;
+            else if (codedOutput == digit_1)
+                decoded[i] = 1;
+            else if (codedOutput == digit_2)
+                decoded[i] = 2;
+            else if (codedOutput == digit_3)
+                decoded[i] = 3;
+            else if (codedOutput == digit_4)
+                decoded[i] = 4;
+            else if (codedOutput == digit_5)
+                decoded[i] = 5;
+            else if (codedOutput == digit_6)
+                decoded[i] = 6;
+            else if (codedOutput == digit_7)
+                decoded[i] = 7;
+            else if (codedOutput == digit_8)
+                decoded[i] = 8;
+            else if (codedOutput == digit_9)
+                decoded[i] = 9;
+            else
+                std::cout << "FUCK!" << '\n';
+        }
+
+        uint16_t decoded_sum = decoded[0] * 1000 + decoded[1] * 100 + decoded[2] * 10 + decoded[3];
+        sum += decoded_sum;
+    }
+
+    output << sum;
     return output.str();
 }
 
