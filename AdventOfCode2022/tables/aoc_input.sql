d4a065a277dc6a15629e4e4d1c3fb5c6a1d7d296
TheDwoon <TheDwoon@users.noreply.github.com>
2022-11-21 23:59:24 +0100

    moved aoc22 to its own repo

--- AdventOfCode2022/tables/aoc_input.sql
+++
@@ -1,8 +0,0 @@
-create global temporary table aoc_input (
-    LINE NUMBER,
-    CONTENT VARCHAR2(4000)
-)
-on commit delete rows
-/
-create unique index aoc_input_u1 on dwieland.aoc_input(LINE);
-/
