4628d7bff65cc6f46f7e8c1643e2fcb9cb6034b3
TheDwoon <TheDwoon@users.noreply.github.com>
2023-12-11 00:42:29 +0100

    implemented basic vec2

---
+++ AdventOfCode2023/vec2.cpp
@@ -0,0 +1,39 @@
+#include <tuple>
+
+template<typename T>
+struct vec2 {
+    T x;
+    T y;
+
+    vec2(T x, T y) : x(x), y(y) {
+
+    }
+
+    vec2& operator+=(const vec2<T>& other) {
+        x += other.x;
+        y += other.y;
+
+        return *this;
+    }
+
+    friend vec2<T> operator+(vec2<T> lhs, const vec2<T> &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+
+    friend bool operator<(const vec2<T> &lhs, const vec2<T> &rhs) {
+        return std::tie(lhs.y, lhs.x) < std::tie(rhs.y, rhs.x);
+    }
+
+    friend bool operator==(const vec2<T> &lhs, const vec2<T> &rhs) {
+        return lhs.y == rhs.y && lhs.x == rhs.x;
+    }
+
+    friend bool operator!=(const vec2<T> &lhs, const vec2<T> &rhs) {
+        return lhs.y != rhs.y || lhs.x != rhs.x;
+    }
+};
+
+typedef vec2<int> vec2i;
+typedef vec2<float> vec2f;
+typedef vec2<double> vec2d;
