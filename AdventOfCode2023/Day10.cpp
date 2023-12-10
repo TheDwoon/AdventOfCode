82fa26a53aedccbdf1ecc9e5742343667b55c471
TheDwoon <TheDwoon@users.noreply.github.com>
2023-12-10 14:52:10 +0100

    setup Day 10

---
+++ AdventOfCode2023/Day10.cpp
@@ -0,0 +1,104 @@
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <sstream>
+#include <iterator>
+#include <vector>
+
+#define TITLE "Day 10"
+
+typedef std::vector<std::string> day_t;
+
+day_t parseInput(std::string &input) {
+    day_t result;
+    return result;
+}
+
+std::string runPart1(day_t& input) {
+    std::stringstream output;
+
+    return output.str();
+}
+
+std::string runPart2(day_t& input) {
+    std::stringstream output;
+
+    return output.str();
+}
+
+// BOILER PLATE CODE BELOW
+
+std::string readInput() {
+    std::cin >> std::noskipws;
+
+    std::istream_iterator<char> it(std::cin);
+    std::istream_iterator<char> end;
+    std::string fileContent(it, end);
+
+    return fileContent;
+}
+
+std::string formatTime(std::chrono::duration<long long, std::nano> t) {
+    std::stringstream output;
+    if (t.count() < 10000) {
+        output << t.count() << "ns";
+    }
+    else if (t.count() < 10000000) {
+        output << t.count() / 1000 << "us";
+    }
+    else {
+        output << t.count() / 1000000 << "ms";
+    }
+
+    return output.str();
+}
+
+int main()
+{
+	std::cout << "######################################" << std::endl;
+	std::cout << "############### " << TITLE << " ###############" << std::endl;
+	std::cout << "######################################" << std::endl;
+
+    auto gt0 = std::chrono::high_resolution_clock::now();
+	const std::string originalInput = readInput();
+    auto gt1 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Reading Input: " << formatTime(gt1 - gt0) << '\n';
+	std::cout << "**************************************\n";
+
+    std::string input = originalInput;
+	auto t0 = std::chrono::high_resolution_clock::now();
+	day_t parsedInput = parseInput(input);
+	auto t1 = std::chrono::high_resolution_clock::now();
+	std::string output = runPart1(parsedInput);
+	auto t2 = std::chrono::high_resolution_clock::now();
+
+	std::cout << '\n';
+	std::cout << "**************************************\n";
+	std::cout << output << '\n';
+	std::cout << "*************** Task 1 ***************\n";
+	std::cout << "Parsing: " << formatTime(t1 - t0) << '\n';
+	std::cout << "Running: " << formatTime(t2 - t1) << '\n';
+	std::cout << "Total: " << formatTime(t2 - t0) << '\n';
+	std::cout << "**************************************" << std::endl;
+
+	input = originalInput;
+	t0 = std::chrono::high_resolution_clock::now();
+	parsedInput = parseInput(input);
+	t1 = std::chrono::high_resolution_clock::now();
+	output = runPart2(parsedInput);
+	t2 = std::chrono::high_resolution_clock::now();
+
+	std::cout << std::endl;
+	std::cout << "**************************************\n";
+	std::cout << output << '\n';
+	std::cout << "*************** Task 2 ***************\n";
+	std::cout << "Parsing: " << formatTime(t1 - t0) << '\n';
+	std::cout << "Running: " << formatTime(t2 - t1) << '\n';
+	std::cout << "Total: " << formatTime(t2 - t0) << '\n';
+	std::cout << "**************************************" << std::endl;
+
+    auto gt2 = std::chrono::high_resolution_clock::now();
+    std::cout << "Global Time: " << formatTime(gt2 - gt0) << '\n';
+	std::cout << "**************************************" << std::endl;
+}
