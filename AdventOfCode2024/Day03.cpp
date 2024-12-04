8cbed9374238a739780d88200c8b4b18f5734630
TheDwoon <TheDwoon@users.noreply.github.com>
2024-12-04 21:34:51 +0100

    performance (maybe not sure)

--- AdventOfCode2024/Day03.cpp
+++ AdventOfCode2024/Day03.cpp
@@ -1,10 +1,4 @@
-#include <string>
-#include <iostream>
-#include <chrono>
-#include <sstream>
-#include <iterator>
 #include <vector>
-#include <algorithm>
 #include "parser.cpp"
 
 #define TITLE "Day 03"
@@ -21,19 +15,11 @@ struct instruction {
     int b;
 };
 
-constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
-
-struct input {
-    char buffer[INPUT_BUFFER_SIZE];
-    long length;
-};
-
-std::vector<std::string> tokenize(const std::string &input, const std::string &separator);
 typedef std::vector<instruction> day_t;
 
-day_t parseInput(const input &input) {
+day_t parseInput(const char* const buffer, const int length) {
     day_t instructions;
-    Parser p(input.buffer);
+    Parser p(buffer);
     int a, b;
     while (!p.eof()) {
         if (p.consume("mul(") && p.readNumber(a) && p.consume(",") && p.readNumber(b) && p.consume(")")) {
@@ -64,62 +50,18 @@ void runDay(const day_t& input) {
         }
     }
 
-    std::cout << "Part 1: " << part1 << '\n';
-    std::cout << "Part 2: " << part2 << '\n';
+    printf("%d\n", part1);
+    printf("%d\n", part2);
 }
 
 // BOILER PLATE CODE BELOW
 
-void readInput(input &i) {
-    std::cin.read(i.buffer, INPUT_BUFFER_SIZE);
-
-    i.length = std::cin.gcount();
-}
-
-std::string formatTime(std::chrono::duration<long long, std::nano> t) {
-    std::stringstream output;
-    if (t.count() < 10000) {
-        output << t.count() << "ns";
-    }
-    else if (t.count() < 10000000) {
-        output << t.count() / 1000 << "us";
-    }
-    else {
-        output << t.count() / 1000000 << "ms";
-    }
-
-    return output.str();
-}
-
 int main()
 {
-#ifdef NDEBUG
-    auto gt0 = std::chrono::high_resolution_clock::now();
-#endif
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
+    char buffer[INPUT_BUFFER_SIZE];
+    const int length = static_cast<int>(fread(buffer, 1, INPUT_BUFFER_SIZE, stdin));
 
-    input in;
-	readInput(in);
-	const day_t parsedInput = parseInput(in);
+	const day_t parsedInput = parseInput(buffer, length);
 	runDay(parsedInput);
-#ifdef NDEBUG
-    auto gt2 = std::chrono::high_resolution_clock::now();
-    std::cout << "Global Time: " << formatTime(gt2 - gt0) << '\n';
-#endif
-}
-
-std::vector<std::string> tokenize(const std::string &input, const std::string &separator) {
-    std::vector<std::string> tokenized;
-
-    size_t pos = 0;
-    size_t tokenEnd;
-    do {
-        tokenEnd = input.find(separator, pos);
-        std::string token = input.substr(pos, (tokenEnd - pos));
-        tokenized.push_back(token);
-        pos = tokenEnd + separator.size();
-    } while(tokenEnd != std::string::npos);
-
-    return tokenized;
 }
