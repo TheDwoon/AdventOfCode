2c7fa8d4b154c00e98ce640f0986cea294970f67
TheDwoon <TheDwoon@users.noreply.github.com>
2024-12-04 21:10:53 +0100

    using c style for IO

--- AdventOfCode2024/Day04.cpp
+++ AdventOfCode2024/Day04.cpp
@@ -1,23 +1,9 @@
 #include <string>
-#include <iostream>
 #include <chrono>
-#include <sstream>
-#include <iterator>
-#include <vector>
-#include <algorithm>
 #include "parser.cpp"
 #include "vec2.cpp"
 
 #define TITLE "Day 04"
-
-constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
-
-struct input {
-    char buffer[INPUT_BUFFER_SIZE];
-    int length;
-};
-
-typedef input day_t;
 
 struct map {
     const char* buffer;
@@ -41,17 +27,17 @@ bool foundString(const char* str, unsigned int length, const vec2i &v, vec2i pos
     return false;
 }
 
-void runDay(const day_t& input) {
+void runDay(const char* const buffer, const int length) {
     int part1 = 0;
     int part2 = 0;
 
 
-    Parser p(input.buffer);
+    Parser p(buffer);
     unsigned int lineLength;
     p.findNext("\n", lineLength);
 
     const vec2i directions[] { {-1, 1}, {0, 1}, {1, 1}, {1, 0}};
-    map m { input.buffer, static_cast<int>(lineLength), static_cast<int>(input.length / (lineLength + 1)), static_cast<int>(lineLength + 1) };
+    map m { buffer, static_cast<int>(lineLength), static_cast<int>(length / (lineLength + 1)), static_cast<int>(lineLength + 1) };
 
     // Part 1
     for (vec2i pos; pos.y < m.height; pos.y++) {
@@ -77,46 +63,16 @@ void runDay(const day_t& input) {
         }
     }
 
-    std::cout << "Part 1: " << part1 << '\n';
-    std::cout << "Part 2: " << part2 << '\n';
+    printf("%d\n",part1);
+    printf("%d\n",part2);
 }
 
 // BOILER PLATE CODE BELOW
 
-void readInput(input &i) {
-    std::cin.read(i.buffer, INPUT_BUFFER_SIZE);
-
-    i.length = static_cast<int>(std::cin.gcount());
-}
-
-std::string formatTime(std::chrono::duration<long long, std::nano> t) {
-    std::stringstream output;
-    if (t.count() < 10000) {
-        output << t.count() << "ns";
-    }
-    else if (t.count() < 10000000) {
-        output << t.count() / 1000 << "us";
-    }
-    else {
-        output << t.count() / 1000000 << "ms";
-    }
-
-    return output.str();
-}
-
 int main()
 {
-#ifdef NDEBUG
-    auto gt0 = std::chrono::high_resolution_clock::now();
-#endif
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    input in;
-	readInput(in);
-	runDay(in);
-#ifdef NDEBUG
-    auto gt2 = std::chrono::high_resolution_clock::now();
-    std::cout << "Global Time: " << formatTime(gt2 - gt0) << '\n';
-#endif
+    constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
+    char buffer[INPUT_BUFFER_SIZE];
+    const int length = static_cast<int>(fread(buffer, 1, INPUT_BUFFER_SIZE, stdin));
+	runDay(buffer, length);
 }
