482dac64abe28019176de4cadf3e47d0cc09ed37
TheDwoon <TheDwoon@users.noreply.github.com>
2024-12-07 23:30:52 +0100

    a still not yet finished Day06 Part 2

--- AdventOfCode2024/Day06.cpp
+++ AdventOfCode2024/Day06.cpp
@@ -1,8 +1,15 @@
 #include <algorithm>
+#include <cstring>
 #include <vector>
 #include "parser.cpp"
 #include "vec2.cpp"
 #include "directions.cpp"
+
+constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
+constexpr char MASK_UP = 1;
+constexpr char MASK_DOWN = 2;
+constexpr char MASK_LEFT = 4;
+constexpr char MASK_RIGHT = 8;
 
 struct map {
     char* buffer;
@@ -11,9 +18,14 @@ struct map {
     const int lineLength;
 
     [[nodiscard]]
+    bool contains(const vec2i& pos) const {
+        return pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height;
+    }
+
+    [[nodiscard]]
     char& at(const int x, const int y) const {
-        assert (pos.x >= 0 && pos.x < width);
-        assert (pos.y >= 0 && pos.y < height);
+        assert (x >= 0 && x < width);
+        assert (y >= 0 && y < height);
         return buffer[y * lineLength + x];
     }
 
@@ -31,8 +43,8 @@ struct map {
     }
 
     char operator()(const int x, const int y) const {
-        assert (pos.x >= 0 && pos.x < width);
-        assert (pos.y >= 0 && pos.y < height);
+        assert (x >= 0 && x < width);
+        assert (y >= 0 && y < height);
         return buffer[y * lineLength + x];
     }
 };
@@ -44,6 +56,34 @@ struct segment {
 
 int signum(const int x) {
     return (x > 0) - (x < 0);
+}
+
+void printMap(const map& m) {
+    for (int y = 0; y < m.height; y++) {
+        for (int x = 0; x < m.width; x++) {
+            char c = m(x, y);
+            if (c == 0)
+                c = ' ';
+            else if (c == '#')
+                c = '#';
+            else if (c == 'O')
+                c = 'O';
+            else if (c & MASK_UP)
+                c = '^';
+            else if (c & MASK_DOWN)
+                c = 'v';
+            else if (c & MASK_LEFT)
+                c = '<';
+            else if (c & MASK_RIGHT)
+                c = '>';
+            else
+                c = '+';
+
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+    printf("=========================\n");
 }
 
 vec2i findStartingPosition(const map& m) {
@@ -58,9 +98,50 @@ vec2i findStartingPosition(const map& m) {
     return {-1, -1};
 }
 
+bool runsInCircle(vec2i pos, vec2i facing, const map &m) {
+    while (pos.x >= 0 && pos.x < m.width && pos.y >= 0 && pos.y < m.height) {
+        char mask;
+        if (facing == aoc::direction::UP) {
+            mask = MASK_UP;
+        } else if (facing == aoc::direction::DOWN) {
+            mask = MASK_DOWN;
+        } else if (facing == aoc::direction::LEFT) {
+            mask = MASK_LEFT;
+        } else {
+            mask = MASK_RIGHT;
+        }
+
+        const char c = m(pos);
+        if ((c & mask) == mask) {
+            // m.at(pos) = 'O';
+            // printMap(m);
+            return true;
+        }
+
+        m.at(pos) = static_cast<char>(c | mask);
+
+        if (m.contains(pos + facing) && m(pos + facing) == '#') {
+            facing = aoc::direction::TURN_RIGHT * facing;
+        } else {
+            pos += facing;
+        }
+    }
+
+    return false;
+}
+
 void runDay(char* const buffer, const int length) {
     int part1 = 0;
     int part2 = 0;
+
+    char originalInput[INPUT_BUFFER_SIZE];
+    memcpy(originalInput, buffer, sizeof(originalInput));
+    for (int i = 0; i < INPUT_BUFFER_SIZE; i++) {
+        if (originalInput[i] != '#')
+            originalInput[i] = 0;
+    }
+
+    char virtualBuffer[INPUT_BUFFER_SIZE];
 
     Parser p(buffer);
     unsigned int lineLength;
@@ -68,58 +149,27 @@ void runDay(char* const buffer, const int length) {
 
     const map m { buffer, static_cast<int>(lineLength), static_cast<int>(length / (lineLength + 1)), static_cast<int>(lineLength + 1) };
 
-
     vec2i facing = aoc::direction::UP;
-    vec2i start = findStartingPosition(m);
-    vec2i pos = start;
+    const vec2i startPos = findStartingPosition(m);
+    vec2i pos = startPos;
 
-    std::vector<segment> segments;
     while (pos.x >= 0 && pos.x < m.width && pos.y >= 0 && pos.y < m.height) {
         m.at(pos) = 'X';
 
-        if (m(pos + facing) == '#') {
+        if (m.contains(pos + facing) && m(pos + facing) == '#') {
             facing = aoc::direction::TURN_RIGHT * facing;
-            segments.emplace_back(start, pos - start);
-            start = pos;
         } else {
-            pos += facing;
-        }
-    }
-    segments.emplace_back(start, pos - start);
-
-
-    for (auto& segment : segments) {
-        printf("(%d, %d) + (%d, %d)\n", segment.pos.x, segment.pos.y, segment.direction.x, segment.direction.y);
-    }
-
-    const int size = static_cast<int>(segments.size());
-    int leftMostFound = 0;
-    for (int i = 1; i < size; i++) {
-        const segment &currentEdge = segments[i];
-        for (int j = leftMostFound; j < i; j++) {
-            const segment &previousEdge = segments[j];
-
-            if (currentEdge.direction * previousEdge.direction == 0
-                && (currentEdge.direction.x != 0 && signum(currentEdge.direction.x) == signum(previousEdge.direction.y)
-                    || currentEdge.direction.y != 0 && signum(currentEdge.direction.y) != signum(previousEdge.direction.x)) ) {
-                // compute easy intersect
-                vec2i intersection = currentEdge.pos;
-                vec2i rockPosition = intersection;
-                if (currentEdge.direction.x == 0) {
-                    int u = (previousEdge.pos.y - currentEdge.pos.y) / signum(currentEdge.direction.y);
-                    intersection.y += u * signum(currentEdge.direction.y);
-                    rockPosition.y += (u + 1) * signum(currentEdge.direction.y);
-                    printf("%d (%d, %d)\n", u, intersection.x, intersection.y);
-                } else {
-                    int u = (previousEdge.pos.x - currentEdge.pos.x) / signum(currentEdge.direction.x);
-                    intersection.x += u * signum(currentEdge.direction.x);
-                    rockPosition.x += (u + 1) * signum(currentEdge.direction.x);
-                    printf("%d (%d, %d)\n", u, intersection.x, intersection.y);
+            // Pretend there is a boulder!
+            memcpy(virtualBuffer, originalInput, sizeof(virtualBuffer));
+            map mVirtual { virtualBuffer, m.width, m.height, m.lineLength };
+            if (mVirtual.contains(pos + facing) && pos + facing != startPos) {
+                mVirtual.at(pos + facing) = '#';
+                if (runsInCircle(pos, facing, mVirtual)) {
+                    part2++;
                 }
-
-                if (m(rockPosition) != '#')
-                    printf("Loopy bit at (%d, %d)\n", intersection.x, intersection.y);
             }
+
+            pos += facing;
         }
     }
 
@@ -139,7 +189,6 @@ void runDay(char* const buffer, const int length) {
 
 int main()
 {
-    constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
     char buffer[INPUT_BUFFER_SIZE];
     const int length = static_cast<int>(fread(buffer, 1, INPUT_BUFFER_SIZE, stdin));
 	runDay(buffer, length);
