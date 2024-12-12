1c45d05c1611fd5f935e361909351e8d0a1affff
TheDwoon <TheDwoon@users.noreply.github.com>
2024-12-12 15:42:24 +0100

    Implemented Day 12 Part1 after properly reading the instructions

--- AdventOfCode2024/Day12.cpp
+++ AdventOfCode2024/Day12.cpp
@@ -1,47 +1,50 @@
 #include <cstring>
-#include "vec2.cpp"
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include "directions.cpp"
+#include "util.cpp"
 #include "parser.cpp"
 
 constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
 
-struct map {
-    char* buffer;
-    const int width;
-    const int height;
-    const int lineLength;
 
-    [[nodiscard]]
-    bool contains(const vec2i& pos) const {
-        return pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height;
+void findCluster(const int x, const int y, const int cluster_number, const aoc::map<char> &plot_map, aoc::map<int> &cluster_map) {
+    std::unordered_set<vec2i> visited;
+    std::deque<vec2i> queue { {x, y} };
+    while (!queue.empty()) {
+        // put cluster number in cluster map
+        const vec2i &v = queue.front();
+        cluster_map.at(v) = cluster_number;
+
+        // expand in all directions if possible
+        const char current_plot = plot_map(v);
+
+        const vec2i up = v + aoc::direction::UP;
+        const vec2i down = v + aoc::direction::DOWN;
+        const vec2i right = v + aoc::direction::RIGHT;
+        const vec2i left = v + aoc::direction::LEFT;
+
+        if (plot_map.contains(up) && plot_map(up) == current_plot && !visited.contains(up)) {
+            visited.insert(up);
+            queue.push_back(up);
+        }
+        if (plot_map.contains(down) && plot_map(down) == current_plot && !visited.contains(down)) {
+            visited.insert(down);
+            queue.push_back(down);
+        }
+        if (plot_map.contains(left) && plot_map(left) == current_plot && !visited.contains(left)) {
+            visited.insert(left);
+            queue.push_back(left);
+        }
+        if (plot_map.contains(right) && plot_map(right) == current_plot && !visited.contains(right)) {
+            visited.insert(right);
+            queue.push_back(right);
+        }
+
+        queue.pop_front();
     }
-
-    [[nodiscard]]
-    char& at(const int x, const int y) const {
-        assert (x >= 0 && x < width);
-        assert (y >= 0 && y < height);
-        return buffer[y * lineLength + x];
-    }
-
-    [[nodiscard]]
-    char& at(const vec2i& pos) const {
-        assert (pos.x >= 0 && pos.x < width);
-        assert (pos.y >= 0 && pos.y < height);
-        return buffer[pos.y * lineLength + pos.x];
-    }
-
-    char operator()(const vec2i& pos) const {
-        assert (pos.x >= 0 && pos.x < width);
-        assert (pos.y >= 0 && pos.y < height);
-        return buffer[pos.y * lineLength + pos.x];
-    }
-
-    char operator()(const int x, const int y) const {
-        assert (x >= 0 && x < width);
-        assert (y >= 0 && y < height);
-        return buffer[y * lineLength + x];
-    }
-};
-
+}
 
 void runDay(char* const buffer, const int length) {
     int part1 = 0;
@@ -52,109 +55,123 @@ void runDay(char* const buffer, const int length) {
     p.findNext("\n", lineLength);
 
     // parse input
-    const map m { buffer, static_cast<int>(lineLength), static_cast<int>(length / (lineLength + 1)), static_cast<int>(lineLength + 1) };
+    const aoc::map m { buffer, static_cast<int>(lineLength), static_cast<int>(length / (lineLength + 1)), static_cast<int>(lineLength + 1) };
 
-    char area[26] { 0 };
-    int perimeter[26] { 0 };
+    int* const cluster_buffer = new int[m.width * m.height];
+    memset(cluster_buffer, 0, m.width * m.height * sizeof(int));
 
-    for (int y = 0; y < m.height - 1; y++) {
-        for (int x = 0; x < m.width - 1; x++) {
-            const char current_plot = m(x, y);
-            const int current_plot_index = current_plot - 'A';
+    aoc::map cluster_map { cluster_buffer, m.width, m.height, m.width  };
 
-            const char next_plot = m(x + 1, y);
-            const char below_plot = m(x, y + 1);
+    int cluster_number = 1;
+    for (int y = 0; y < m.height; ++y) {
+        for (int x = 0; x < m.width; ++x) {
+            if (cluster_map(x, y) == 0) {
+                findCluster(x, y, cluster_number++, m, cluster_map);
+            }
+        }
+    }
+#ifndef NDEBUG
+    for (int y = 0; y < m.height; ++y) {
+        for (int x = 0; x < m.width; ++x) {
+            printf("%2d ", cluster_map(x, y));
+        }
+        printf("\n");
+    }
+    printf("\n");
+#endif
 
-            area[current_plot_index] += 1;
+    std::unordered_map<int, int> cluster_area;
+    std::unordered_map<int, int> cluster_perimeter;
 
-            if (current_plot != next_plot) {
-                const int next_plot_index = next_plot - 'A';
+    for (int y = 0; y < cluster_map.height - 1; y++) {
+        for (int x = 0; x < cluster_map.width - 1; x++) {
+            const int current_cluster = cluster_map(x, y);
 
-                perimeter[current_plot_index] += 1;
-                perimeter[next_plot_index] += 1;
+            const int next_cluster = cluster_map(x + 1, y);
+            const int below_cluster = cluster_map(x, y + 1);
+
+            cluster_area[current_cluster] += 1;
+
+            if (current_cluster != next_cluster) {
+                cluster_perimeter[current_cluster] += 1;
+                cluster_perimeter[next_cluster] += 1;
             }
 
-            if (current_plot != below_plot) {
-                const int below_plot_index = below_plot - 'A';
-
-                perimeter[current_plot_index] += 1;
-                perimeter[below_plot_index] += 1;
+            if (current_cluster != below_cluster) {
+                cluster_perimeter[current_cluster] += 1;
+                cluster_perimeter[below_cluster] += 1;
             }
         }
     }
 
     // top edge & bottom edge
-    for (int x = 0; x < m.width; x++) {
-        const char top_plot = m(x, 0);
-        const char bottom_plot = m(x, m.height - 1);
+    for (int x = 0; x < cluster_map.width; x++) {
+        const int top_cluster = cluster_map(x, 0);
+        const int bottom_cluster = cluster_map(x, cluster_map.height - 1);
 
         // area not accounted for in main loop
-        area[bottom_plot - 'A'] += 1;
+        cluster_area[bottom_cluster] += 1;
 
         // account for outer perimeter
-        perimeter[top_plot - 'A'] += 1;
-        perimeter[bottom_plot - 'A'] += 1;
+        cluster_perimeter[top_cluster] += 1;
+        cluster_perimeter[bottom_cluster] += 1;
     }
 
     // left edge & right edge
-    for (int y = 0; y < m.height; y++) {
-        const char left_plot = m(0, y);
-        const char right_plot = m(m.width - 1, y);
+    for (int y = 0; y < cluster_map.height; y++) {
+        const int left_cluster = cluster_map(0, y);
+        const int right_cluster = cluster_map(cluster_map.width - 1, y);
 
         // area not accounted for in main loop
-        area[right_plot - 'A'] += 1;
+        cluster_area[right_cluster] += 1;
 
         // account for outer perimeter
-        perimeter[left_plot - 'A'] += 1;
-        perimeter[right_plot - 'A'] += 1;
+        cluster_perimeter[left_cluster] += 1;
+        cluster_perimeter[right_cluster] += 1;
     }
 
     // incremented by both top and right edge loop
-    area[m(m.width - 1, m.height - 1) - 'A'] -= 1;
+    cluster_area[cluster_map(m.width - 1, m.height - 1)] -= 1;
 
     // right most horizontal edge segments missed by main loop
-    for (int y = 0; y < m.height - 1; y++) {
-        const char current_plot = m(m.width - 1, y);
-        const char bottom_plot = m(m.width - 1, y + 1);
+    for (int y = 0; y < cluster_map.height - 1; y++) {
+        const int current_cluster = cluster_map(cluster_map.width - 1, y);
+        const int bottom_cluster = cluster_map(cluster_map.width - 1, y + 1);
 
-        if (current_plot != bottom_plot) {
-            const int current_plot_index = current_plot - 'A';
-            const int bottom_plot_index = bottom_plot - 'A';
-
-            perimeter[current_plot_index] += 1;
-            perimeter[bottom_plot_index] += 1;
+        if (current_cluster != bottom_cluster) {
+            cluster_perimeter[current_cluster] += 1;
+            cluster_perimeter[bottom_cluster] += 1;
         }
     }
 
     // bottom most vertical edge segments missed by main loop
-    for (int x = 0; x < m.width - 1; x++) {
-        const char current_plot = m(x, m.height - 1);
-        const char next_plot = m(x + 1, m.height - 1);
+    for (int x = 0; x < cluster_map.width - 1; x++) {
+        const int current_cluster = cluster_map(x, cluster_map.height - 1);
+        const int next_cluster = cluster_map(x + 1, cluster_map.height - 1);
 
-        if (current_plot != next_plot) {
-            const int current_plot_index = current_plot - 'A';
-            const int next_plot_index = next_plot - 'A';
-
-            perimeter[current_plot_index] += 1;
-            perimeter[next_plot_index] += 1;
+        if (current_cluster != next_cluster) {
+            cluster_perimeter[current_cluster] += 1;
+            cluster_perimeter[next_cluster] += 1;
         }
     }
 
 #ifndef NDEBUG
     printf("Areas:\n");
-    for (int i = 0; i < 26; i++) {
-        if (area[i] > 0) {
-            printf("%c: %3d\n", i + 'A', area[i]);
-        }
+    for (auto const& area : cluster_area) {
+        printf("%3d: %3d\n", area.first, area.second);
     }
 
     printf("Perimeter:\n");
-    for (int i = 0; i < 26; i++) {
-        if (perimeter[i] > 0) {
-            printf("%c: %3d\n", i + 'A', perimeter[i]);
-        }
+    for (auto const& perimeter : cluster_perimeter) {
+        printf("%3d: %3d\n", perimeter.first, perimeter.second);
     }
 #endif
+
+    for (auto [cluster_number, area] : cluster_area) {
+        const int perimeter = cluster_perimeter[cluster_number];
+
+        part1 += area * perimeter;
+    }
 
     printf("%d\n", part1);
     printf("%d\n", part2);
--- AdventOfCode2024/util.cpp
+++ AdventOfCode2024/util.cpp
@@ -68,206 +68,41 @@ namespace aoc {
     }
 
     template<typename T>
-    struct map2d {
-        std::vector<T> data;
-        vec2i topLeft;
-        int width{0};
-        int height{0};
+    struct map {
+        T* buffer;
+        const int width;
+        const int height;
+        const int lineLength;
 
-        map2d() = default;
-
-        map2d(vec2i topLeft, int width, int height) : topLeft(topLeft), width(width), height(height) {
-            data.resize(width * height);
+        [[nodiscard]]
+        bool contains(const vec2i& pos) const {
+            return pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height;
         }
 
-        map2d(int width, int height, T init = 0) : width(width), height(height) {
-            data.resize(width * height, init);
+        [[nodiscard]]
+        T& at(const int x, const int y) const {
+            assert (x >= 0 && x < width);
+            assert (y >= 0 && y < height);
+            return buffer[y * lineLength + x];
         }
 
-        [[nodiscard]] bool contains(const vec2i& v) const {
-            return contains(v.x, v.y);
+        [[nodiscard]]
+        T& at(const vec2i& pos) const {
+            assert (pos.x >= 0 && pos.x < width);
+            assert (pos.y >= 0 && pos.y < height);
+            return buffer[pos.y * lineLength + pos.x];
         }
 
-        [[nodiscard]] bool contains(int x, int y) const {
-            x -= topLeft.x;
-            y -= topLeft.y;
-            return x >= 0 && x < width && y >= 0 && y < height;
+        T operator()(const vec2i& pos) const {
+            assert (pos.x >= 0 && pos.x < width);
+            assert (pos.y >= 0 && pos.y < height);
+            return buffer[pos.y * lineLength + pos.x];
         }
 
-        void resize(const vec2i& newOrigin, int newWidth, int newHeight, T init = 0) {
-            std::vector<T> newData;
-            newData.resize(newWidth * newHeight, init);
-
-            int dx = topLeft.x - newOrigin.x;
-            int dy = topLeft.y - newOrigin.y;
-
-            for (int y = std::max(0, -dy); y < std::min(height, newHeight - dy); y++) {
-                for (int x = std::max(0, -dx); x < std::min(width, newWidth - dx); x++) {
-                    newData[(y + dy) * newWidth + x + dx] = data[y * width + x];
-                }
-            }
-
-            topLeft = newOrigin;
-            width = newWidth;
-            height = newHeight;
-            data = newData;
-        }
-
-        void extend(vec2i direction, T init = 0) {
-            int newWidth = width;
-            int newHeight = height;
-            vec2i newOrigin = topLeft;
-
-            newWidth += std::abs(direction.x);
-            newHeight += std::abs(direction.y);
-            if (direction.x < 0) {
-                newOrigin.x += direction.x;
-            }
-            if (direction.y < 0) {
-                newOrigin.y += direction.y;
-            }
-
-            resize(newOrigin, newWidth, newHeight, init);
-        }
-
-        void include(const vec2i &position, int buffer = 0, T init = 0) {
-            if (contains(position)) return;
-
-            vec2i delta = position - topLeft;
-            delta.x += signum(delta.x) * buffer;
-            delta.y += signum(delta.y) * buffer;
-            if (delta.x >= width)
-                delta.x -= width;
-            else if (delta.x > 0)
-                delta.x = 0;
-
-            if (delta.y >= height)
-                delta.y -= height;
-            else if (delta.y > 0)
-                delta.y = 0;
-
-            extend(delta, init);
-        }
-
-        void copy(const map2d<T> &other, const vec2i& position, T init = 0) {
-            vec2i bottomRight = position + vec2i(other.width - 1, other.height - 1);
-
-            include(position, 0, init);
-            include(bottomRight, 0, init);
-
-            for (int y = 0; y < other.height; y++) {
-                for (int x = 0; x < other.width; x++) {
-                    data[(position.y + y - topLeft.y) * width + position.x + x - topLeft.x] = other.data[y * other.width + x];
-                }
-            }
-        }
-
-        void origin(const vec2i& origin) {
-            topLeft -= origin;
-        }
-
-        void center() {
-            vec2i newCenter {(topLeft.x + width) / 2, (topLeft.y + height) / 2};
-            origin(newCenter);
-        }
-
-        [[nodiscard]] int minX() const {
-            return topLeft.x;
-        }
-
-        [[nodiscard]] int maxX() const {
-            return topLeft.x + width;
-        }
-
-        [[nodiscard]] int minY() const {
-            return topLeft.y;
-        }
-
-        [[nodiscard]] int maxY() const {
-            return topLeft.y + height;
-        }
-
-        T& operator()(const vec2i &v) {
-            assert(contains(v.x, v.y));
-            return data[(v.y - topLeft.y) * width + v.x - topLeft.x];
-        }
-
-        const T& operator()(const vec2i &v) const {
-            return data[(v.y - topLeft.y) * width + v.x - topLeft.x];
-        }
-
-        T& operator()(int x, int y) {
-            return data[(y - topLeft.y) * width + x - topLeft.x];
-        }
-
-        const T& operator()(int x, int y) const {
-            return data[(y - topLeft.y) * width + x - topLeft.x];
-        }
-    };
-
-    typedef map2d<int> map2di;
-    typedef map2d<char> map2dc;
-
-    template<typename T>
-    struct map2d_view {
-        const map2d<T> *map;
-        vec2i offset;
-
-        map2d_view(const map2d<T> *map, const vec2i &offset) : map(map), offset(offset) {
-            assert(map != nullptr);
-        }
-
-        [[nodiscard]] bool contains(vec2i pos) const {
-            pos += offset;
-            return map->contains(pos);
-        }
-
-        const T& operator()(vec2i pos) const {
-            pos += offset;
-            return (*map)(pos);
-        }
-    };
-
-    typedef map2d_view<int> map2di_view;
-    typedef map2d_view<char> map2dc_view;
-
-    template<typename TNode, typename TEdge>
-    struct edge;
-
-    template<typename TNode, typename TEdge>
-    class node {
-    public:
-        node() = default;
-        node(TNode value): value(value) {
-
-        }
-
-        TNode value;
-        std::vector<std::shared_ptr<edge<TNode, TEdge>>> edges_out;
-        std::vector<std::shared_ptr<edge<TNode, TEdge>>> edges_in;
-    };
-
-    template<typename TNode, typename TEdge>
-    struct edge {
-        std::shared_ptr<node<TNode, TEdge>> source;
-        std::shared_ptr<node<TNode, TEdge>> target;
-        TEdge value;
-    };
-
-    template<typename TNode = int, typename TEdge = int>
-    struct directed_graph {
-        std::vector<std::shared_ptr<node<TNode, TEdge>>> nodes;
-
-        std::shared_ptr<node<TNode, TEdge>> addNode(TNode value) {
-            std::shared_ptr<node<TNode, TEdge>> n = std::make_shared<node<TNode, TEdge>>(value);
-            nodes.push_back(n);
-            return n;
-        }
-
-        void addEdge(std::shared_ptr<node<TNode, TEdge>>& source, std::shared_ptr<node<TNode, TEdge>>& target, TEdge value) {
-            std::shared_ptr<edge<TNode, TEdge>> e = std::make_shared<edge<TNode, TEdge>>(source, target, value);
-            source->edges_out.push_back(e);
-            target->edges_in.push_back(e);
+        T operator()(const int x, const int y) const {
+            assert (x >= 0 && x < width);
+            assert (y >= 0 && y < height);
+            return buffer[y * lineLength + x];
         }
     };
 }
