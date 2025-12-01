5f46cae132c391954486cabfaa22983e41b62f4c
Daniel Wieland <daniel.wieland@promatis.de>
2025-12-01 18:27:29 +0100

    implemented Day 01

--- AdventOfCode2025/Day01.cpp
+++ AdventOfCode2025/Day01.cpp
@@ -7,6 +7,46 @@ void runDay(char* const buffer, const int length) {
     int part1 = 0;
     int part2 = 0;
 
+    int dial = 50;
+
+    Parser p(buffer);
+    while (!p.eof())
+    {
+        int delta;
+        switch (p.read())
+        {
+        case 'L':
+            p.readNumber(delta);
+            p.readNewLine();
+
+            delta *= -1;
+            break;
+        case 'R':
+            p.readNumber(delta);
+            p.readNewLine();
+            break;
+        default:
+            throw std::exception();
+        }
+
+        if (delta > 0)
+        {
+            part2 += (delta + dial) / 100;
+        }
+        else if (delta < 0)
+        {
+            part2 += dial == 0 ? delta / -100  : (delta - (100 - dial)) / -100;
+        }
+
+        dial += delta;
+        while (dial < 0)
+            dial += 100;
+        while (dial > 99)
+            dial -= 100;
+
+        part1 += dial == 0;
+    }
+
     printf("%d\n",part1);
     printf("%d\n",part2);
 }
