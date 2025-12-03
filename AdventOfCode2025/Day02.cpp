a601d61c0048aaa53ffa808d27dec049f96c0823
TheDwoon <TheDwoon@users.noreply.github.com>
2025-12-03 22:18:44 +0100

    implemented Day02

--- AdventOfCode2025/Day02.cpp
+++ AdventOfCode2025/Day02.cpp
@@ -1,14 +1,95 @@
 #include <cstring>
+#include <vector>
+#include <cmath>
+#include <cstdint>
+#include <pstl/execution_defs.h>
+
 #include "parser.cpp"
 
 constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
 
-void runDay(char* const buffer, const int length) {
-    int part1 = 0;
-    int part2 = 0;
+struct range
+{
+    uint64_t start;
+    uint64_t end;
+};
 
-    printf("%d\n",part1);
-    printf("%d\n",part2);
+bool isFakeId(const uint64_t number)
+{
+    const int magnitude = static_cast<int>(std::log10(number)) + 1;
+    if (magnitude % 2 == 1)
+        return false;
+
+    const int sword = static_cast<int>(std::pow(10, magnitude / 2));
+    return number / sword == number % sword;
+}
+
+bool sliceNumber(uint64_t number, const int slice)
+{
+    const uint64_t pattern = number % slice;
+    number /= slice;
+    while (number > 0)
+    {
+        if (number % slice != pattern)
+            return false;
+
+        number /= slice;
+    }
+
+    return true;
+}
+
+bool isAdvancedFake(const uint64_t number)
+{
+    const int magnitude = static_cast<int>(std::log10(number)) + 1;
+    int swordMagnitude = magnitude / 2;
+    int sword = static_cast<int>(std::pow(10, swordMagnitude));
+    while (sword > 1)
+    {
+        if (magnitude % swordMagnitude == 0 && sliceNumber(number, sword))
+            return true;
+
+        swordMagnitude--;
+        sword /= 10;
+    }
+
+    return false;
+}
+
+void runDay(char* const buffer, const int length) {
+    uint64_t part1 = 0;
+    uint64_t part2 = 0;
+
+    std::vector<range> ranges;
+    Parser p(buffer);
+    while (!p.isNewLine())
+    {
+        range r;
+        p.readNumber(r.start);
+        p.consume('-');
+        p.readNumber(r.end);
+        p.consume(',');
+        ranges.push_back(r);
+    }
+
+    for (const auto& r : ranges)
+    {
+        for (uint64_t i = r.start; i <= r.end; ++i)
+        {
+            if (isFakeId(i))
+            {
+                part1 += i;
+                part2 += i;
+            }
+            else if (isAdvancedFake(i))
+            {
+                part2 += i;
+            }
+        }
+    }
+
+    printf("%llu\n",part1);
+    printf("%llu\n",part2);
 }
 
 // BOILER PLATE CODE BELOW
