3075d198f9370d7db10ba85f432ff02e45f3a826
TheDwoon <TheDwoon@users.noreply.github.com>
2025-12-05 22:09:15 +0100

    implemented Day05

---
+++ AdventOfCode2025/Day05.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <cstdint>
+#include <cstring>
+#include <vector>
+#include <set>
+
+#include "parser.cpp"
+
+constexpr long INPUT_BUFFER_SIZE = 32 * 1024;
+
+struct range
+{
+    uint64_t start;
+    uint64_t end;
+};
+
+bool operator<(const range& lhs, const range& rhs) { return lhs.start < rhs.start; }
+
+void runDay(char* const buffer, const int length) {
+    int part1 = 0;
+    uint64_t part2 = 0;
+
+    std::vector<range> validRanges;
+    std::vector<uint64_t> numbers;
+
+    Parser p(buffer);
+    while (!p.isNewLine())
+    {
+        range r;
+        p.readNumber(r.start);
+        p.consume('-');
+        p.readNumber(r.end);
+        p.readNewLine();
+        validRanges.push_back(r);
+    }
+
+    p.readNewLine();
+    while (!p.eof())
+    {
+        uint64_t n;
+        p.readNumber(n);
+        p.readNewLine();
+        numbers.push_back(n);
+    }
+
+    std::sort(validRanges.begin(), validRanges.end());
+    std::vector<range> distinctValidRanges;
+
+    range currentRange = validRanges[0];
+    for (size_t i = 1; i < validRanges.size(); ++i)
+    {
+        if (validRanges[i].start <= currentRange.end)
+        {
+            currentRange.start = std::min(currentRange.start, validRanges[i].start);
+            currentRange.end = std::max(currentRange.end, validRanges[i].end);
+        } else
+        {
+            distinctValidRanges.push_back(currentRange);
+            currentRange = validRanges[i];
+        }
+    }
+
+    distinctValidRanges.push_back(currentRange);
+
+    for (const auto&n : numbers)
+    {
+        const range search = { n, n };
+        auto it = std::lower_bound(distinctValidRanges.cbegin(), distinctValidRanges.cend(), search);
+        if (it != distinctValidRanges.cbegin())
+            --it;
+
+        if (it->start <= n && n <= it->end)
+        {
+            part1 += 1;
+        }
+    }
+
+    for (const auto& r : distinctValidRanges)
+    {
+        part2 += r.end - r.start + 1;
+    }
+
+    printf("%d\n",part1);
+    printf("%llu\n",part2);
+}
+
+// BOILER PLATE CODE BELOW
+
+int main()
+{
+    char buffer[INPUT_BUFFER_SIZE] {};
+    const int length = static_cast<int>(fread(buffer, 1, INPUT_BUFFER_SIZE, stdin));
+	runDay(buffer, length);
+}
